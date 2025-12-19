c65a462ee938c5eca9e6bec62ae0bfc46b349163
idilks <idil.k.sahin.26@dartmouth.edu>
2025-12-19 15:24:37 +0100

    debugging picture logic

---
+++ slurm_scripts/pic_array.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=pic_array      # create a short name for your job
+#SBATCH --nodes=1                 # node count
+#SBATCH --ntasks=1                # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1         # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --gres=gpu:1              # number of gpus per node
+#SBATCH --time=5:59:00            # total run time limit (HH:MM:SS)
+#SBATCH --partition=ood
+#SBATCH --mem=500GB               # total memory
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=idil.k.sahin.26@dartmouth.edu
+#SBATCH --array=0-5
+#SBATCH --output=logs/pic_array_%A_%a.out
+#SBATCH --error=logs/pic_array_%A_%a.err
+
+export PIP_CACHE_DIR="/dartfs/rc/lab/F/FranklandS/.pip/cache"
+export TRANSFORMERS_CACHE="/dartfs/rc/lab/F/FranklandS/models/cache"
+export HF_HOME="/dartfs/rc/lab/F/FranklandS/models/cache"
+export HF_DATASETS_CACHE="/dartfs/rc/lab/F/FranklandS/models/cache"
+export HF_METRICS_CACHE="/dartfs/rc/lab/F/FranklandS/models/cache"
+export WANDB_API_KEY="bd1c08839d0c8c49e7c3efe9aabe2d9c644befb6"
+
+source /optnfs/common/miniconda3/etc/profile.d/conda.sh
+cd
+mkdir -p .conda/pkgs/cache .conda/envs
+
+cd /dartfs/rc/lab/F/FranklandS/tom
+conda activate /dartfs/rc/lab/F/FranklandS/tom/envs/tom_analysis
+
+TEMPLATES=("food_truck" "food_truck" "hair_styling" "hair_styling" "basic_object_move_detailed" "basic_object_move_detailed")
+PATCH_FLAGS=("" "--patch_after_movement" "" "--patch_after_movement" "" "--patch_after_movement")
+
+python codebase/tasks/identity_rules/cma.py \
+  --use_behavioral_tom \
+  --context_type photo \
+  --base_rule ABA \
+  --template_names ${TEMPLATES[$SLURM_ARRAY_TASK_ID]} \
+  --prompt_num 50 \
+  --max_new_tokens 5 \
+  --activation_name z \
+  --model_type Qwen2.5-14B-Instruct \
+  --question_style instruction \
+  --better_cma \
+  ${PATCH_FLAGS[$SLURM_ARRAY_TASK_ID]}
